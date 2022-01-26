195bc1b1ee45b17b381bb052951eb03844507551
CreaColorBlume <creacolorangelique@gmail.com>
2022-01-26 10:53:45 +0100

    movement lets goooo

--- Still-Trying-Game/Assets/Skripts/PlayerMovement.cs
+++ Still-Trying-Game/Assets/Skripts/PlayerMovement.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    Rigidbody rb;
+    [SerializeField] float movementSpeed = 6f;
+    [SerializeField] float jumpForce = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+
+
+
     }
+
 }
