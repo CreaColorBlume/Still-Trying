a2ed0297704ab518907dc849630ff764257c4c8b
CreaColorBlume <creacolorangelique@gmail.com>
2022-01-27 16:04:31 +0100

    enemy

---
+++ Still-Trying-Game/Assets/Skripts/PlayerLife.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+
+public class PlayerLife : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy Body"))
+        {
+            Die();
+
+        }
+    }
+
+    void Die()
+    {
+        GetComponent<MeshRenderer>().enabled = false;
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<PlayerMovement>().enabled = false;
+
+        Invoke(nameof(ReloadLeveL),1.3f);
+
+    }
+
+    void ReloadLeveL()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
+
+
